c555125ba3ea8bc8e091e3377646888f0b00224a
Manuel Ortiz Cacha <manuel.ortiz.cacha@ingka.ikea.com>
2022-06-10 10:49:01 +0200

    New flask project boilerplate

---
+++ tests/apis/test_tweet_views.py
@@ -0,0 +1,13 @@
+from flask_testing import TestCase
+from app import create_app
+
+class TestTweetView(TestCase):
+    def create_app(self):
+        app = create_app()
+        app.config['TESTING'] = True
+        return app
+
+    def test_tweet(self):
+        response = self.client.get("/tweets/hello")
+        text = response.data.decode()
+        self.assertIn("Hello from the 'tweets' namespace!", text)
